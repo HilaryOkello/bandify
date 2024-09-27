554120bdbc6e27186f30d2ff6be8aac36b47d7d9
hilaokello <hilaryokello091@gmail.com>
2024-09-27 17:35:30 +0300

    moved structs to models.go

--- server/utils.go
+++ server/utils.go
@@ -17,30 +17,6 @@ const (
 	relationsURL = "https://groupietrackers.herokuapp.com/api/relation"
 )
 
-type Artist struct {
-	Id           int      `json:"id"`
-	Image        string   `json:"image"`
-	Name         string   `json:"name"`
-	Members      []string `json:"members"`
-	CreationDate int      `json:"creationDate"`
-	FirstAlbum   string   `json:"firstAlbum"`
-	Locations    string   `json:"locations"`
-	ConcertDates string   `json:"concertDates"`
-	Relations    string   `json:"relations"`
-}
-
-type Location struct {
-	Location string `json:"location"`
-}
-type Date struct {
-	Date string `json:"date"`
-}
-type Relation struct {
-	ArtistID   int `json:"artistId"`
-	LocationID int `json:"locationId"`
-	DateID     int `json:"dateId"`
-}
-
 var artists []Artist
 
 var templates map[string]*template.Template
